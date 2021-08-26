6889a19ae956f407aa6b6ba9d205df9bbfe4e085
Jay Patel <81924044+jayp822@users.noreply.github.com>
2021-08-26 12:42:21 -0400

    Add files via upload

---
+++ ReverseAString.java
@@ -0,0 +1,20 @@
+package KATA;
+
+import java.util.Scanner;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		char[] arr = text.toCharArray();
+
+		// your code goes here
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.print(arr[i]);
+		}
+
+	}
+
+}
